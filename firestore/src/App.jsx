312272eb2eb72bd0a0bd7f7c355e8a9f00981163
Jennifer Zhuang <118019266+jzhuang03@users.noreply.github.com>
2024-05-21 12:12:54 -0400

    Display all previously submitted responses

--- firestore/src/App.jsx
+++ firestore/src/App.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { db } from "../firebase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 
 function App() {
   const [answer, setAnswer] = useState("");
+  const [submittedAnswers, setSubmittedAnswers] = useState([]);
+
+  {
+    /* Take the answers that the user has submitted*/
+  }
+  useEffect(() => {
+    const fetchAnswers = async () => {
+      const returnAnswer = await getDocs(collection(db, "answers"));
+      const answers = returnAnswer.docs.map((doc) => doc.data());
+      setSubmittedAnswers(answers);
+    };
+
+    fetchAnswers();
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -12,6 +26,15 @@ function App() {
       answer: answer,
     });
     console.log("created doc with id: ", docRef.id);
+
+    {
+      /* For each answer that the user has submitted, assign ID to the answer for display purposes */
+    }
+    setSubmittedAnswers([
+      ...submittedAnswers,
+      { id: docRef.id, answer: answer },
+    ]);
+    setAnswer("");
   };
 
   return (
@@ -19,10 +42,24 @@ function App() {
       <h1>Welcome!</h1>
       <form onSubmit={handleSubmit}>
         <label> Answer:</label>
-        <input type="text" onChange={(e) => setAnswer(e.target.value)}></input>
+        <input
+          type="text"
+          value={answer}
+          onChange={(e) => setAnswer(e.target.value)}
+        ></input>
         <br></br>
         <button type="submit">Submit</button>
       </form>
+
+      {/* Display the answers that the user has submitted*/}
+      <div className="display">
+        <h2>Submitted Answers:</h2>
+        <ul>
+          {submittedAnswers.map((submittedAnswer) => (
+            <li key={submittedAnswer.id}>{submittedAnswer.answer}</li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 }
