3330eb933b8ee4888a010a1e01b009000c283c9c
Jennifer Zhuang <118019266+jzhuang03@users.noreply.github.com>
2024-05-21 11:17:27 -0400

    correctly created the form

--- firestore/src/App.jsx
+++ firestore/src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [answer, setAnswer] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(answer);
+  };
 
   return (
     <>
-<h1> Hello! </h1>
+      <h1> Hello! </h1>
+      <h2> What tasks do you need to do today? </h2>
+      <form onSubmit={handleSubmit}>
+        <label> Answer: </label>
+        {/* With onChange, keep track of user changing their answers*/}
+        <input type="text" onChange={(e) => setAnswer(e.target.value)}></input>
+        <br />
+        <button type="submit"> Submit </button>
+      </form>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
